1823d7b21b527c46ac9dcd8d091f96392f7ba036
paolo-della-fera <paolodf2005@gmail.com>
2026-03-20 17:36:10 +0100

    add pages with cards of all the products taken from API

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,11 +1,50 @@
+import { useEffect, useState } from "react"
+
+
 export default function Prodotti() {
+    const [prodotti, setProdotti] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            .then(data => setProdotti(data))
+    })
+
     return (
         <>
-            
+
             <main>
-                <h1>Prodotti</h1>
+
+                {/* card prodotti */}
+                <div className="container">
+
+                    <h1 className="text-center mt-3">I nostri Prodotti</h1>
+
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 mt-5">
+
+                        {prodotti.map(prodotto =>
+                            <div className="col" key={prodotto.id}>
+                                <div className="card h-100">
+                                    <img src={prodotto.image} className="card-img-top p-2" alt={prodotto.title} />
+                                    <div className="card-body">
+                                        <span className="badge bg-secondary mb-2 text-uppercase">
+                                            {prodotto.category}
+                                        </span>
+                                        <h5 className="card-title">{prodotto.title}</h5>
+                                        <p className="card-text text-muted">{prodotto.description}</p>
+                                        <p className="fw-bold fs-5">${prodotto.price}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+
+                    </div>
+
+                </div>
+
+
             </main>
-           
+
         </>
     )
 }
